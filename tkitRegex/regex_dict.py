64e77e6d4cfcd0dd7788b79dd834523453d2f402
Terry Chan <napoler2008@gmail.com>
2022-09-06 18:18:29 +0800

    添加

--- tkitRegex/regex_dict.py
+++ tkitRegex/regex_dict.py
@@ -7,23 +7,6 @@ REGEX_DICT={}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 REGEX_DICT['email'] = {
     "regex":r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+",
     "type":"replace",
